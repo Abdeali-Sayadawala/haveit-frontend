7f2516ded05df94dd809dd42dd94f04a8a6c9e9f
SaiNikhila98 <159783011+SaiNikhila98@users.noreply.github.com>
2024-02-27 08:24:00 +0000

    changes in homepage

--- src/Components/HomePage/Homepage.jsx
+++ src/Components/HomePage/Homepage.jsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
+import './Homepage.css';
 
 export default function Homepage() {
     return(
-        <div></div>
+        <div className='menu_class'>
+            <div className='categories_main'>
+                Categories
+            </div>
+            <div className='categories_div'></div>
+        </div>
     );
 
 }
