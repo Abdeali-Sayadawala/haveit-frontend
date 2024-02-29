f9e670b3e5bae23ed95ffe761125f936666e018a
Abdeali Sayadawala <abdeali009@gmail.com>
2024-02-29 16:17:24 +0530

    navigation bar changes responsive

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
-import hamburger from '../Assets/icons8-hamburger-192.png';
 import logopng from '../Assets/logo-png.png';
 import './Navigation.css';
-import { NavLink } from 'react-router-dom';
+import phone from '../Assets/phone-504.svg';
+import location_pointer from '../Assets/location-pointer-2961.svg';
+// import { NavLink } from 'react-router-dom';
 // import NavigationScript from './NavigationScript.js';
 
 const Navigation = () => {
 
-    const toggleNav = () => {
-        const navbar = document.querySelector(".navbar");
-        navbar.classList.toggle("show");
-    }
+    // const toggleNav = () => {
+    //     const navbar = document.querySelector(".navbar");
+    //     navbar.classList.toggle("show");
+    // }
 
-    const CloseNav = (event) => {
-        const navbar = document.querySelector(".navbar");
+    // const CloseNav = (event) => {
+    //     const navbar = document.querySelector(".navbar");
 
-        let position = event.offsetTop - 90;
-        window.scrollTo({
-            left: 0,
-            top: position,
-            behavior: 'smooth',
-          });
-        navbar.classList.remove('show');
+    //     let position = event.offsetTop - 90;
+    //     window.scrollTo({
+    //         left: 0,
+    //         top: position,
+    //         behavior: 'smooth',
+    //       });
+    //     navbar.classList.remove('show');
 
-        return true;
-    }
+    //     return true;
+    // }
 
     return (
         <div>
@@ -32,14 +33,21 @@ const Navigation = () => {
                 <a href="/" className="logo">
                     <img src={logopng} alt=""/>
                 </a>
-                <nav className="navbar">
-                    <NavLink to="/menu" onClick={CloseNav}>Menu</NavLink>
-                    <NavLink to="/address" onClick={CloseNav} >Address</NavLink>
-                    <NavLink to="/contact" onClick={CloseNav} >Contact Us</NavLink>
-                </nav>
-                <div className="hamburger" onClick={toggleNav}>
-                    <img src={hamburger} alt="" />
+                <div className='current_info'>
+                    <div className='address_bar'>2, Rambhau Bhogle Marg, Anjeer Wadi, Mustafa Bazar, Mazgaon</div>
+                    <div className='open_indication active'>Open</div>
+                    <div className='close_indication'>Closed</div>
                 </div>
+                <nav className="navbar">
+                    <a href="#" className='nav_icon location'><img src={phone} alt=''/></a>
+                    <a href="tel:9867164753" className='nav_icon call'><img src={location_pointer} alt=''/></a>
+                    {/* <NavLink to="/menu" onClick={CloseNav}>Menu</NavLink>
+                    <NavLink to="/address" onClick={CloseNav} >Address</NavLink>
+                    <NavLink to="/contact" onClick={CloseNav} >Contact Us</NavLink> */}
+                </nav>
+                {/* <div className="hamburger" onClick={toggleNav}>
+                    <img src={hamburger} alt="" />
+                </div> */}
             </header>
         </div>
     );
